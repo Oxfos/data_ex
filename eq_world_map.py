a4fe77cb60063737400bff602ee861875d862320
oxfos <rinaldo_bertossa@hotmail.com>
2020-02-19 19:26:57 +0100

    Refactored code.

--- eq_world_map.py
+++ eq_world_map.py
@@ -10,21 +10,21 @@ with open(filename) as f:
 
 all_eq_dicts = all_eq_data['features']
 
-mags, lons, lats = [], [], []
+mags, lons, lats, hover_texts = [], [], [], []
 
 for eq_dict in all_eq_dicts:
-    mag = eq_dict['properties']['mag']
-    lon = eq_dict['geometry']['coordinates'][0]
-    lat = eq_dict['geometry']['coordinates'][1]
-    mags.append(mag)
-    lons.append(lon)
-    lats.append(lat)
-
+    mags.append(eq_dict['properties']['mag'])
+    lons.append(eq_dict['geometry']['coordinates'][0])
+    lats.append(eq_dict['geometry']['coordinates'][1])
+    hover_texts.append(eq_dict['properties']['title'])
+# Getting the title from the file
+data_title = all_eq_data["metadata"]["title"]
 # Map the earthquackes.
 data = [{
     'type': 'scattergeo',
     'lon': lons,
     'lat': lats,
+    'text': hover_texts,
     'marker': {
         'size': [3*mag for mag in mags],
         'color': mags,
@@ -33,7 +33,6 @@ data = [{
         'colorbar': {'title': 'Magnitude'},
     }
     }]
-my_layout = Layout(title='Global Earthquackes')
-
+my_layout = Layout(title=data_title)
 fig = {'data': data, 'layout': my_layout}
 offline.plot(fig, filename='global_earthquackes.html')
