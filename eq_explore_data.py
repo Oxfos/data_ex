125c5f7185509d69a63f085eb2c2c77bc79eddfb
oxfos <rinaldo_bertossa@hotmail.com>
2020-02-19 07:09:48 +0100

    Extracting coordinates into tuples.

--- eq_explore_data.py
+++ eq_explore_data.py
@@ -4,6 +4,18 @@ import json
 filename = 'data/eq_data_1_day_m1.json'
 with open(filename) as f:
     all_eq_data = json.load(f) # data converted into dictionary.
-readable_file = 'data/readable_eq_data.json'
-with open(readable_file, 'w') as f:
-    json.dump(all_eq_data, f, indent=4)
+
+all_eq_dicts = all_eq_data['features']
+
+mags = []
+for eq_dict in all_eq_dicts:
+    mag = eq_dict['properties']['mag']
+    mags.append(mag)
+
+location = []
+for eq_dict in all_eq_dicts:
+    coord = (eq_dict['geometry']['coordinates'][0], 
+        eq_dict['geometry']['coordinates'][1])
+    location.append(coord)
+
+print(location[:10])
